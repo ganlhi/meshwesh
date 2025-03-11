abff1bba5ba777375aef778f10d07c0518e4e29d
Guilhem Brouat <guilhem.brouat@heetch.com>
2025-03-11 14:25:02 +0100

    wip: Home

---
+++ app/components/LayoutHeader.tsx
@@ -0,0 +1,13 @@
+import styles from './LayoutHeader.module.css'
+import Image from "next/image";
+import banner from '@/app/images/meshwesh-banner.png'
+import Link from "next/link";
+
+export function LayoutHeader() {
+
+  return <header className={styles.header}>
+    <Link href='/'>
+    <Image src={banner} alt='Meshwesh' /></Link>
+  </header>
+
+}
